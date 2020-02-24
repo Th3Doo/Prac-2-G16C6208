869d837de6a829c7e79b543fd9bc6277aeed1aa5
Th3Doo <scottiec4rock@gmail.com>
2020-02-24 12:54:15 +0200

    Committed all .csproj, .cs and .sln files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,51 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        
+        string Prefix(string input)
+        {
+            if (input == "")
+            {
+                return ("0,0:");
+            }
+            else
+            {
+                int iLength = input.Length;
+                int iWords = 0;
+                int count = 0;
+                while (count < iLength)
+                {
+                    if (input[0] == ' ')
+                    {
+                        while (input[count + 1] == ' ')
+                        {
+                            count++;
+                        }
+                        count++;
+                    }
+                    if (input[count] ==' ')
+                    {
+                        iWords++;
+                        while (input[count + 1] == ' ')
+                        {
+                            count++;
+                        }
+                    }
+                    count++;
+                }
+                if (input[iLength - 1] != ' ')
+                {
+                    iWords++;
+                }
+                return (iLength.ToString() + "," + iWords.ToString() + ":" + input);
+            }
+            
         }
     }
 }
